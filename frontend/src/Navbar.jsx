2d300d7996d747c13a0ba2747ab14d0301d4dbcb
Yameen <yameenmunir05@gmail.com>
2025-03-03 15:31:08 +0000

    Test Commit

---
+++ frontend/src/Navbar.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+function Navbar() {
+    return (
+        <nav className="navbar">
+            <div className="logo">🔥 Fog & Flame</div>
+            <ul className="nav-links">
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/reservation">Reservation</Link>
+                </li>
+                <li>
+                    <Link to="/contact">Contact</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
