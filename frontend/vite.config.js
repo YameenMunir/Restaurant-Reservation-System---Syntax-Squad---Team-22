a82f15c5d1736c1e4e75dc3150605e66e8b3a285
Yameen Munir <74267033+YameenMunir@users.noreply.github.com>
2025-04-08 18:04:41 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Add the base option with the name of your repository
+  base: '/Restaurant-Reservation-System---Syntax-Squad---Team-22/',
 })
